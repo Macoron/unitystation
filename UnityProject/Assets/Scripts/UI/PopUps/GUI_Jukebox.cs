c8efa6419566d539dd6d5228b4c6f436e038aef8
Alex Evgrashin <aevgrashin@yandex.ru>
2020-04-18 13:44:50 +0300

    Started  wokring on jukebox ui

---
+++ UnityProject/Assets/Scripts/UI/PopUps/GUI_Jukebox.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_Jukebox : NetTab
+{
+
+	//Close the screen.
+	public void CloseDialog()
+	{
+		ControlTabs.CloseTab(Type, Provider);
+	}
+}
