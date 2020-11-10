3ec4f6edb150b6001e05b4620d0713d49c9c2ccf
Alex Evgrashin <aevgrashin@yandex.ru>
2020-11-11 01:44:04 +0300

    Started working on candles

---
+++ UnityProject/Assets/Scripts/Items/Tool/Lighting/Candle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CandleState
+{
+	public SpriteDataSO litState;
+	public SpriteDataSO unlitState;
+}
+
+public class Candle : MonoBehaviour
+{
+	public SpriteHandler spriteHandler;
+	public CandleState[] spriteStates;
+
+	private Flamable flamable = null;
+	private FireSource fireSource = null;
+
+	[SerializeField]
+	[Tooltip("Time for candle to burn-out in seconds")]
+	private float maxBurningTimeSeconds = 2000;
+	private float burningTimeSeconds;
+
+	private void Awake()
+	{
+		flamable = GetComponent<Flamable>();
+		fireSource = GetComponent<FireSource>();
+	}
+}
